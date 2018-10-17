36a9e0583694d287a2f630aaccef00ae1f25005d
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-10-17 14:27:30 +0200

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,29 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+
+
+
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("501507420690841600")
+setInterval(function() {
+channel.send(`gfgffffffffffffhdl,fgbkl;jgl;kjhk;;;lfdfhlmاحلا مسااااااااااااااااااااا :joy:`);
+}, 25)
+})
+
+
+
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN2);
